2432400c97eba75b0b2918f59949561ad48bd54a
Fabio Stegmeyer <fabio.stegmeyer@gmail.com>
2023-12-18 00:44:55 +0100

    Refactor mobile nav to offcanvas

--- packages/site-package/Resources/Public/JavaScript/header.js
+++ packages/site-package/Resources/Public/JavaScript/header.js
@@ -1,16 +1,17 @@
 const header = document.querySelector("#page-header");
 const contentWrap = document.querySelector("#page-content");
+const offcanvas = document.querySelector("#offcanvasNavigaton");
+const offcanvasToggle = document.querySelector("#offcanvasNavigatonToggle");
 
-console.log(header);
-console.log(contentWrap);
 window.addEventListener("load", adjustBodyTopPadding);
 window.addEventListener("resize", adjustBodyTopPadding);
 
 function adjustBodyTopPadding(e) {
   let paddingTop = header.offsetHeight;
-  console.log(paddingTop);
   contentWrap.style.paddingTop = paddingTop + "px";
+  offcanvas.style.paddingTop = paddingTop + "px";
 }
 
-
-
+offcanvasToggle.onclick = function () {
+  offcanvasToggle.classList.toggle("active");
+};
