0c9892e378a1ed5d4690afed9505d2664825d36f
Fabio Stegmeyer <fabio.stegmeyer@gmail.com>
2023-12-18 16:58:57 +0100

    Update header scroll behaviour

--- packages/site-package/Resources/Public/JavaScript/header.js
+++ packages/site-package/Resources/Public/JavaScript/header.js
@@ -2,12 +2,11 @@ const header = document.querySelector("#page-header");
 const contentWrap = document.querySelector("#page-content");
 const offcanvas = document.querySelector("#offcanvasNavigaton");
 const offcanvasToggle = document.querySelector("#offcanvasNavigatonToggle");
-
+const paddingTop = header.offsetHeight;
 window.addEventListener("load", adjustBodyTopPadding);
 window.addEventListener("resize", adjustBodyTopPadding);
 
 function adjustBodyTopPadding(e) {
-  let paddingTop = header.offsetHeight;
   contentWrap.style.paddingTop = paddingTop + "px";
   offcanvas.style.paddingTop = paddingTop + "px";
 }
@@ -15,3 +14,20 @@ function adjustBodyTopPadding(e) {
 offcanvasToggle.onclick = function () {
   offcanvasToggle.classList.toggle("active");
 };
+
+/**
+ * Add transition class
+ */
+window.addEventListener("DOMContentLoaded", function () {
+  var n = document.querySelectorAll(".navbar-fixed-top");
+  function t() {
+    paddingTop / 2 < window.scrollY
+      ? n[0].classList.add("navbar-transition")
+      : n[0].classList.remove("navbar-transition");
+  }
+  1 <= n.length &&
+    (["scroll", "resize", "DOMContentLoaded"].forEach(function (n) {
+      window.addEventListener(n, t);
+    }),
+    t());
+});
